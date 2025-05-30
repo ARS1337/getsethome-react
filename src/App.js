69d273c4587aa0b1225b9b1133e8140e3f714d73
abhaySingh17498 <135430958+abhaySingh17498@users.noreply.github.com>
2025-05-30 19:43:27 +0530

    added list of links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import BillPayments from "./pages/BillPayments";
 import Landing from "./pages/Landing";
 import RentPayments from "./pages/RentPayments";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
-  // return <BillPayments/>;
-  // return <Landing/>
-  return <RentPayments/>
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/bill-payments" element={<BillPayments />} />
+        <Route path="/rent-payments" element={<RentPayments />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
